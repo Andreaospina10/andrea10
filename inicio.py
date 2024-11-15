7918ac51cd4df78e6cfdcc8037682538ef0aa96d
Andrea Ospina <andreaospinaolaya@gmail.com>
2024-11-14 21:05:51 -0500

    hola

---
+++ inicio.py
@@ -0,0 +1,40 @@
+print ("Hola Aprendices ADSO 2774163")
+
+#argumentos de print
+print("hola","mundo","python",3)
+print("Adios mundo python")
+
+#print con un separador
+print("hoy","es","jueves",sep="*")
+
+#variables
+x=15
+print("tipo de dato de x:",type(x))
+y=2
+print("tipo de dato de y:",type(y))
+z = x/y
+print(z)
+print("tipo de dato de z:",type(z))
+x= "hola"
+print(x)
+print(type(x))
+w= True
+print("tipo de dato de w:",type(w))
+
+#impresiones con operaciones
+print(5/2)
+print(5%2)
+print(5**2)
+print(15//4)
+
+
+nombre = input("cual es su nombre ")
+apellidos = input ("cuales son sus apellidos")
+edad = input ("cual es su edad")
+genero=input ("cual es sugenero")
+print ("su nombre es: ", nombre)
+print ("sus apellidos son: ", apellidos)
+print ("su edad es: ", edad)
+print ("que genero eres: ", genero)
+
+
